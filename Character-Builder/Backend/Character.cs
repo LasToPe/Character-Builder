88baf61843c3851a45e32023af20fae7953f91d0
LasToPe <lassep95@gmail.com>
2018-01-16 14:05:57 +0100

    Starting on race, and character almost ready

--- Character-Builder/Backend/Character.cs
+++ Character-Builder/Backend/Character.cs
@@ -5,13 +5,17 @@ namespace Backend
 {
     public class Character
     {
-        public int Strength_Score { get => Strength_Score; set => Strength_Score = value; }
-        public int Dexterity_Score { get => Dexterity_Score; set => Dexterity_Score = value; }
-        public int Constitution_Score { get => Constitution_Score; set => Constitution_Score = value; }
-        public int Intelligence_Score { get => Intelligence_Score; set => Intelligence_Score = value; }
-        public int Wisdom_Score { get => Wisdom_Score; set => Wisdom_Score = value; }
-        public int Charisma_Score { get => Charisma_Score; set => Charisma_Score = value; }
+        public string Name { get; set; }
 
+        //Ability Scores
+        public int Strength_Score { get; set; }
+        public int Dexterity_Score { get; set; }
+        public int Constitution_Score { get; set; }
+        public int Intelligence_Score { get; set; }
+        public int Wisdom_Score { get; set; }
+        public int Charisma_Score { get; set; }
+
+        //Ability Modifiers
         public int Strength_Modifier { get => (Strength_Score - 10) / 2; }
         public int Dexterity_Modifier { get => (Dexterity_Score - 10) / 2; }
         public int Constitution_Modifier { get => (Constitution_Score - 10) / 2; }
@@ -19,7 +23,48 @@ namespace Backend
         public int Wisdom_Modifier { get => (Wisdom_Score - 10) / 2; }
         public int Charisma_Modifier { get => (Charisma_Score - 10) / 2; }
 
-        public Race.Race Race { get => Race; set => Race = value; }
-        public Class.Class Favored_Class { get => Favored_Class; set => Favored_Class = value; }
+        //Temporary Ability Scores
+        public int Temporary_Strength_Score { get; set; }
+        public int Temporary_Dexterity_Score { get; set; }
+        public int Temporary_Constitution_Score { get; set; }
+        public int Temporary_Intelligence_Score { get; set; }
+        public int Temporary_Wisdom_Score { get; set; }
+        public int Temporary_Charisma_Score { get; set; }
+
+        //Temporary Ability Modifiers
+        public int Temporary_Strength_Modifier { get => (Temporary_Strength_Score - 10) / 2; }
+        public int Temporary_Dexterity_Modifier { get => (Temporary_Dexterity_Score - 10) / 2; }
+        public int Temporary_Constitution_Modifier { get => (Temporary_Constitution_Score - 10) / 2; }
+        public int Temporary_Intelligence_Modifier { get => (Temporary_Intelligence_Score - 10) / 2; }
+        public int Temporary_Wisdom_Modifier { get => (Temporary_Wisdom_Score - 10) / 2; }
+        public int Temporary_Charisma_Modifier { get => (Temporary_Charisma_Score - 10) / 2; }
+
+        public Race.Race Race { get; set; }
+        public Class.Class Favored_Class { get; set; }
+
+        public int Hit_Points { get; set; } //set needs change to accomodate automatic calculation
+        
+        public int Initiative { get; set; }
+
+        //Armor Class
+        public int Armor_Class { get; set; } //needs update when armor is implemented
+        public int Touch_AC { get; set; } //needs update when armor is implemented
+        public int FlatFooted_AC { get; set; } //needs update when armor is implemented
+
+        //Saving Throw Bonuses
+        public int Fortitude { get; set; }
+        public int Reflex { get; set; }
+        public int Will { get; set; }
+
+        //Base Attack Bonus and Combat Maneuver Bonuses
+        public int BAB { get; set; }
+        public int CMB { get; set; }
+        public int CMD { get; set; }
+
+        //Skills
+        //public Skills Skills { get; set; } //Not ready yet
+
+        //Feats
+        //public Feats Feats { get; set; } //Not ready yet
     }
 }
--- Character-Builder/Backend/Race/Race.cs
+++ Character-Builder/Backend/Race/Race.cs
@@ -6,5 +6,21 @@ namespace Backend.Race
 {
     public abstract class Race
     {
+        public abstract int Strength_Bonus { get; set; }
+        public abstract int Dexterity_Bonus { get; set; }
+        public abstract int Constitution_Bonus { get; set; }
+        public abstract int Intelligence_Bonus { get; set; }
+        public abstract int Wisdom_Bonus { get; set; }
+        public abstract int Charisma_Bonus { get; set; }
+
+        //public abstract Size Size { get; set; } //Size not ready
+
+        //public abstract Type Type { get; set; } //Type not ready
+
+        public abstract int Base_Speed { get; set;}
+
+        public abstract List<string> Langauges { get; set; }
+
+        //public abstract List<Racial_Trait> Racial_Traits { get; set; } //Racial traits not ready
     }
 }
